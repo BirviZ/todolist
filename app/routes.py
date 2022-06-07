b38564b9f2320e22b42a69be58d0af9d56665707
Serhii Deineko <birvizkozak@gmail.com>
2022-06-07 14:11:06 +0300

    Complete addForm

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import AddForm
 
+
+todos = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = AddForm()
+    return render_template('index.html', form=form, todos=todos)
+
+
+@app.route('/add_todo', methods=['POST'])
+def add_todo():
+    form = AddForm()
+    if not form.validate_on_submit():
+        return redirect(url_for('index'))
+    todos.append(form.body.data)
+    return redirect(url_for('index'))
