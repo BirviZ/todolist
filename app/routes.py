b7bfa27287a4c7523b06344fdff8e2c357384f5d
Serhii Deineko <birvizkozak@gmail.com>
2022-06-07 23:25:05 +0300

    .

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, redirect, url_for
 from app import app
+from app import db
+from app.models import Todo
 from app.forms import AddForm
 
 
@@ -8,7 +10,10 @@ todos = []
 @app.route('/')
 def index():
     form = AddForm()
-    return render_template('index.html', form=form, todos=todos)
+    counters = {'todos': 0, 'active': 0, 'complete': 0}
+    todos = Todo.query.all()
+    counters['todos'] = len(todos)
+    return render_template('index.html', form=form, todos=todos, counters=counters)
 
 
 @app.route('/add_todo', methods=['POST'])
@@ -16,5 +21,8 @@ def add_todo():
     form = AddForm()
     if not form.validate_on_submit():
         return redirect(url_for('index'))
-    todos.append(form.body.data)
+    #todos.append(form.body.data)
+    todo = Todo(body=form.body.data)
+    db.session.add(todo)
+    db.session.commit()
     return redirect(url_for('index'))
