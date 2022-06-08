b4689c676b3d61f74ab0280906b5865f98ce039f
Serhii Deineko <birvizkozak@gmail.com>
2022-06-08 11:23:55 +0300

    Main logic

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,10 @@ def index():
     counters = {'todos': 0, 'active': 0, 'complete': 0}
     todos = Todo.query.all()
     counters['todos'] = len(todos)
+    for todo in todos:
+        if todo.isComplete:
+            counters['complete'] += 1
+    counters['active'] = counters['todos'] - counters['complete']
     return render_template('index.html', form=form, todos=todos, counters=counters)
 
 
@@ -23,6 +27,23 @@ def add_todo():
         return redirect(url_for('index'))
     #todos.append(form.body.data)
     todo = Todo(body=form.body.data)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    todo = Todo.query.get(id)
+    todo.isComplete = True
     db.session.add(todo)
     db.session.commit()
     return redirect(url_for('index'))
